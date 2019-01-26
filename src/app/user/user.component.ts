533f21b588d96783c4bbac286844500edb4b19f9
mmaau <m1macher@edu.aau.at>
2019-01-26 02:51:48 +0100

    user edit
    todo disable button if no change
    http req

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'app-user',
@@ -8,8 +10,9 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class UserComponent implements OnInit {
 
-  constructor(public jwtHelper: JwtHelperService) { }
-
+  constructor(public jwtHelper: JwtHelperService, private fb: FormBuilder ) { }
+  
+  form: FormGroup;
   username: String;
   email: String;
   role: String;
@@ -18,6 +21,11 @@ export class UserComponent implements OnInit {
   // picture: String;
 
   ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.email,Validators.required]]
+
+    });
     const token = sessionStorage.getItem('token');
     const payload = this.jwtHelper.decodeToken(token);
     this.username = payload.user;
@@ -25,4 +33,14 @@ export class UserComponent implements OnInit {
     this.role = payload.role;
   }
 
+   /*
+    *TODO form value when nothing change = "", "" , accept button must be disabled when user do no changes
+    *make an http req to the server with new data and update db + refresh the token 
+  */
+ 
+  changeUser(){
+      console.log(this.form.value);
+
+  }
+ 
 }
