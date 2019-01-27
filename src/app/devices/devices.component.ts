f5da08bb57b1940b66b5cd2f781fbff95634667a
mmaau <m1macher@edu.aau.at>
2019-01-27 03:18:46 +0100

    device add

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -4,6 +4,8 @@ import { Device } from '../services/device';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-devices',
@@ -13,11 +15,15 @@ import { Observable } from 'rxjs';
 
 export class DevicesComponent implements OnInit {
 
+  form: FormGroup;
   devices$: Observable<Device[]>;
   rooms: Set<String> = new Set;
+  category: Set<String> = new Set;
+  module_types: Array<String>  = ["SONOFF_BASIC","SONOFF_RF","SONOFF_LED", "SONOFF_4CH"];
   isOn: Boolean;
+  openAddBox: Boolean = false;
 
-  constructor(private device_service: DeviceService, private _router: Router, public jwtHelper: JwtHelperService) { }
+  constructor(private device_service: DeviceService, private _router: Router, public jwtHelper: JwtHelperService, private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.devices$ = this.device_service.getAllDevices();
@@ -27,7 +33,45 @@ export class DevicesComponent implements OnInit {
       });
       // console.log(this.rooms);
     });
+    
+    
+    this.form = this.fb.group({
+      device_name: ['', []],
+      room_name: ['', []],
+      category: ['', []],
+      device_ip: ['', []],
+      device_mac: ['', []],
+
+
+    });
   }
+
+  openBox(){
+    console.log(this.openAddBox)
+    this.openAddBox = !this.openAddBox;
+  }
+
+  addDevice(){
+    console.log( this.form.value);
+    const url = 'http://localhost:3000/device/addDevice';
+    this.http.post<any>(url, this.form.value, )
+    .subscribe( res => {
+
+    }, err => {
+
+
+    });
+  }
+
+
+
+
+
+  
+
+
+
+
 
 }
 
--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -9,19 +9,20 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatTableModule} from '@angular/material/table';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
 
 // Importing all necessary Material Modules API Reference - https://material.angular.io
 @NgModule({
   imports: [MatToolbarModule, MatButtonModule, MatCardModule, MatSidenavModule,
     MatSlideToggleModule, MatTableModule, MatExpansionModule, MatProgressSpinnerModule,
     MatIconModule, MatListModule, MatInputModule, MatFormFieldModule,
-    MatSnackBarModule, MatTabsModule, MatGridListModule
+    MatSnackBarModule, MatTabsModule, MatGridListModule, MatSelectModule
   ],
 
   exports: [MatToolbarModule, MatButtonModule, MatCardModule, MatSidenavModule,
     MatSlideToggleModule, MatTableModule, MatExpansionModule, MatProgressSpinnerModule,
      MatIconModule, MatListModule, MatInputModule, MatFormFieldModule, MatSnackBarModule,
-      MatTabsModule, MatGridListModule
+      MatTabsModule, MatGridListModule, MatSelectModule
   ],
 })
 
