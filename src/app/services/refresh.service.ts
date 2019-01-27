b201cbee6b7aee36a79fd9863d762b3969398555
Th3Dilli <dilli.1822@gmx.at>
2019-01-27 23:49:25 +0100

    added ng refresh service
    removed refresh from server temp

---
+++ src/app/services/refresh.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RefreshService {
+
+  private _url = 'http://localhost:3000/devices/refresh';
+
+  constructor(private http: HttpClient) { }
+  refresh() {
+    this.http.get(this._url).subscribe(res => {
+      let devices = res.body.devices;
+    }, error => {
+
+    });
+
+  }
+}
