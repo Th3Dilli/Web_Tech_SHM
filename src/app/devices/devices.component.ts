ca3fdec042e32cd3740a9de6856f0fa639017f36
mmaau <m1macher@edu.aau.at>
2019-01-31 16:40:00 +0100

    form validation device add
    fixed the form validation for device add form

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -4,8 +4,9 @@ import { Device } from '../services/device';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-devices',
@@ -21,13 +22,15 @@ export class DevicesComponent implements OnInit {
   module_types: Array<String> = ['SONOFF_BASIC', 'SONOFF_TOUCH', 'SONOFF_4CH'];
   isOn: Boolean;
   openAddBox: Boolean = false;
+  private formSubmitAttempt: boolean;
 
   constructor(
     private device_service: DeviceService,
     private _router: Router,
     public jwtHelper: JwtHelperService,
     private fb: FormBuilder,
-    private http: HttpClient
+    private http: HttpClient,
+    public snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
@@ -40,11 +43,11 @@ export class DevicesComponent implements OnInit {
     });
 
     this.form = this.fb.group({
-      device_name: ['', []],
-      room_name: ['', []],
-      module_type: ['', []],
-      device_ip: ['', []],
-      device_mac: ['', []],
+      device_name: ['', [Validators.required, Validators.maxLength(45)]],
+      room_name: ['', [Validators.required, Validators.maxLength(45)]],
+      module_type: ['', [Validators.required, Validators.maxLength(45)]],
+      device_ip: ['', [Validators.required, Validators.maxLength(15)]],
+      device_mac: ['', [Validators.required, Validators.maxLength(17)]],
     });
   }
 
@@ -53,16 +56,28 @@ export class DevicesComponent implements OnInit {
     this.openAddBox = !this.openAddBox;
   }
 
-  addDevice() {
-    console.log(this.form.value);
-    const url = 'http://localhost:3000/devices/addDevice';
-    this.http.post<any>(url, this.form.value)
-      .subscribe(res => {
-        console.log(res);
-        this.devices$ = this.device_service.getAllDevices();
-      }, error => {
-        console.log(error);
-      });
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
   }
+
+  addDevice() {
+    if (this.form.valid) {
+      const url = 'http://localhost:3000/devices/addDevice';
+      this.http.post<any>(url, this.form.value)
+        .subscribe(res => {
+          this.snackBar.open('Device successfully added', 'Okay', { duration: 3000 });
+          this.devices$ = this.device_service.getAllDevices();
+          this.formSubmitAttempt = false;
+        }, error => {
+          this.snackBar.open('Failed to Add Device', 'Okay', { duration: 3000 });
+        });
+    }
+      this.formSubmitAttempt = true;
+    
+  }
+
 
 }
